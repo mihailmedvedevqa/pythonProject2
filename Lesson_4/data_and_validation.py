3f6c0c71515ea548a641810cb6234991e59c28a8
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-09-13 21:21:51 +0300

    Obtaining and validating page data

---
+++ Lesson_4/data_and_validation.py
@@ -0,0 +1,22 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://www.wikipedia.org/")
+
+PAGE_URL = driver.current_url
+print(f"Page URL: {PAGE_URL}")
+assert PAGE_URL == "https://www.wikipedia.org/", "URL error, wrong page opened"
+
+PAGE_TITLE = driver.title
+print(f"Page Title: {PAGE_TITLE}")
+assert PAGE_TITLE == "Wikipedia", "Incorrect page title"
+
+print(driver.page_source)
+
+time.sleep(3)
