b5e99aff0146bfe10ec5a1f19aaec150048eecbe
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-09-19 21:57:09 +0300

    Alerts handling

---
+++ lesson_13/alerts_handling.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+wait = WebDriverWait(driver, 15, poll_frequency=1)
+
+driver.get("https://demoqa.com/alerts")
+# BUTTON_1 = driver.find_element("xpath", "//button[@id='alertButton']")
+# wait.until(EC.element_to_be_clickable(BUTTON_1)).click()
+# alert = wait.until(EC.alert_is_present())
+# driver.switch_to.alert
+# alert.accept()
+BUTTON_3 = driver.find_element("xpath", "//button[@id='confirmButton']")
+wait.until(EC.element_to_be_clickable(BUTTON_3)).click()
+alert = wait.until(EC.alert_is_present())
+driver.switch_to.alert
+alert.dismiss()
