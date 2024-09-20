2442bc3b24e519d34f35cfc04a9f72e3018d7029
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-09-20 19:13:01 +0300

    Alerts handling

--- lesson_13/alerts_handling.py
+++ lesson_13/alerts_handling.py
@@ -10,13 +10,27 @@ driver = webdriver.Chrome(service=service)
 wait = WebDriverWait(driver, 15, poll_frequency=1)
 
 driver.get("https://demoqa.com/alerts")
-# BUTTON_1 = driver.find_element("xpath", "//button[@id='alertButton']")
-# wait.until(EC.element_to_be_clickable(BUTTON_1)).click()
-# alert = wait.until(EC.alert_is_present())
-# driver.switch_to.alert
-# alert.accept()
+BUTTON_1 = driver.find_element("xpath", "//button[@id='alertButton']")
+wait.until(EC.element_to_be_clickable(BUTTON_1)).click()
+alert = wait.until(EC.alert_is_present())
+driver.switch_to.alert
+alert.accept()
+
 BUTTON_3 = driver.find_element("xpath", "//button[@id='confirmButton']")
 wait.until(EC.element_to_be_clickable(BUTTON_3)).click()
 alert = wait.until(EC.alert_is_present())
 driver.switch_to.alert
 alert.dismiss()
+
+BUTTON_4 = ("xpath", "//button[@id='promtButton']")
+wait.until(EC.element_to_be_clickable(BUTTON_4)).click()
+alert = wait.until(EC.alert_is_present())
+driver.switch_to.alert
+print(alert.text)
+
+# BUTTON_4 = ("xpath", "//button[@id='promtButton']")
+# wait.until(EC.element_to_be_clickable(BUTTON_4)).click()
+# alert = wait.until(EC.alert_is_present())
+# driver.switch_to.alert
+# alert.send_keys("Hello world")
+# alert.accept()
