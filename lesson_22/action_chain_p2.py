848f21742705976e6e236870ba798bfba52c8405
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-09-30 21:49:17 +0300

    Action chain

---
+++ lesson_22/action_chain_p2.py
@@ -0,0 +1,66 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--window-size=1920,1080")
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+wait = WebDriverWait(driver, 10, poll_frequency=1)
+actions = ActionChains(driver)
+
+
+OUTLINE_BUTTON_LOCATOR = ("xpath", "//button[text()='Click Me']")
+
+driver.get("https://demoqa.com/buttons")
+BUTTON = driver.find_element(*OUTLINE_BUTTON_LOCATOR)
+
+actions.click_and_hold(BUTTON)
+actions.perform()
+
+
+SOURCE_LOCATOR = ("xpath", "//div[@id='draggable']")
+TARGET_LOCATOR = ("xpath", "//div[@id='droppable']")
+
+driver.get("https://demoqa.com/droppable")
+
+SOURCE = driver.find_element(*SOURCE_LOCATOR)
+TARGET = driver.find_element(*TARGET_LOCATOR)
+
+wait.until(EC.element_to_be_clickable(SOURCE))
+actions.drag_and_drop(SOURCE, TARGET)
+actions.perform()
+
+SOURCE_LOCATOR = ("xpath", "//div[contains(@class, 'vertical-list')]/div[1]")
+TARGET_LOCATOR = ("xpath", "//div[contains(@class, 'vertical-list')]/div[5]")
+
+driver.get("https://demoqa.com/sortable")
+
+
+def drag_and_drop(source, target):
+    source = driver.find_element(*source)  # Находим source-элемент
+    target = driver.find_element(*target)  # Находим target-элемент
+    wait.until(EC.element_to_be_clickable(source))  # Ждем кликабельности source-элемента
+    actions.drag_and_drop(source, target)
+    actions.perform()
+
+
+drag_and_drop(SOURCE_LOCATOR, TARGET_LOCATOR)  # Использование функции
+
+
+driver.get("https://tympanus.net/Development/DragDropInteractions/index.html")
+
+source = driver.find_element("xpath", "//div[@class='grid__item'][7]")  # Что перетаскиваем
+target = driver.find_element("xpath", "//div[@class='drop-area__item'][2]")  # Куда перетаскиваем
+
+actions.click_and_hold(source)
+actions.pause(2)
+actions.move_to_element(target)
+actions.release()
+actions.perform()
+
