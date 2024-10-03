b65563d632d28fdd29d4c1d3fb7504ae987f81a7
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-10-03 18:16:53 +0300

    Action chain part 2, comments added

--- lesson_22/action_chain_p2.py
+++ lesson_22/action_chain_p2.py
@@ -6,59 +6,80 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
 
+# Создаем объект Options для настройки Chrome
 options = Options()
-options.add_argument("--window-size=1920,1080")
-
+options.add_argument("--window-size=1920,1080")  # Устанавливаем размер окна браузера
+# Устанавливаем ChromeDriver через менеджер и создаем сервис
 service = Service(ChromeDriverManager().install())
+# Инициализируем веб-драйвер для Chrome с заданными сервисом и опциями
 driver = webdriver.Chrome(service=service, options=options)
+# Устанавливаем ожидание до 10 секунд для элементов на странице
 wait = WebDriverWait(driver, 10, poll_frequency=1)
-actions = ActionChains(driver)
+# Создаем объект ActionChains для выполнения действий с элементами на странице
+action = ActionChains(driver)
 
 # Клик с удержанием
-OUTLINE_BUTTON_LOCATOR = ("xpath", "//button[text()='Click Me']")
 
+# Локатор элемента
+OUTLINE_BUTTON_LOCATOR = ("xpath", "//button[text()='Click Me']")
+# Открываем целевую страницу
 driver.get("https://demoqa.com/buttons")
+# Находим элемент на странице с помощью заданного локатора
 BUTTON = driver.find_element(*OUTLINE_BUTTON_LOCATOR)
-actions.click_and_hold(BUTTON)
-actions.perform()
+# Удерживаем кнопку мыши на элементе
+action.click_and_hold(BUTTON)
+action.perform()
 
 # Перетаскивание / drag and drop
-# Вариант 1
-SOURCE_LOCATOR = ("xpath", "//div[@id='draggable']")
-TARGET_LOCATOR = ("xpath", "//div[@id='droppable']")
 
+# Вариант 1
+SOURCE_LOCATOR = ("xpath", "//div[@id='draggable']")  # Локатор для элемента, который будем перетаскивать
+TARGET_LOCATOR = ("xpath", "//div[@id='droppable']")  # Локатор для элемента, куда будем перетаскивать
+# Открываем целевую страницу
 driver.get("https://demoqa.com/droppable")
+# Находим элементы
 SOURCE = driver.find_element(*SOURCE_LOCATOR)
 TARGET = driver.find_element(*TARGET_LOCATOR)
+# Ожидаем, пока элемент не станет кликабельным
 wait.until(EC.element_to_be_clickable(SOURCE))
-actions.drag_and_drop(SOURCE, TARGET)
-actions.perform()
+# Перетаскиваем SOURCE в TARGET
+action.drag_and_drop(SOURCE, TARGET)
+action.perform()
 
-# Вариант 1
+# Вариант 2
+
+# Локатор для элемента, который будем перетаскивать
 SOURCE_LOCATOR = ("xpath", "//div[contains(@class, 'vertical-list')]/div[1]")
+# Локатор для элемента, куда будем перетаскивать
 TARGET_LOCATOR = ("xpath", "//div[contains(@class, 'vertical-list')]/div[5]")
-
+# Открываем целевую страницу
 driver.get("https://demoqa.com/sortable")
 
 
+# Функция для перетаскивания элемента
 def drag_and_drop(source_element, target_element):
     source_element = driver.find_element(*source_element)  # Находим source-элемент
     target_element = driver.find_element(*target_element)  # Находим target-элемент
-    wait.until(EC.element_to_be_clickable(source_element))  # Ждем кликабельности source-элемента
-    actions.drag_and_drop(source_element, target_element)
-    actions.perform()
+    wait.until(EC.element_to_be_clickable(source_element))  # Ждем кликабельности source элемента
+    action.drag_and_drop(source_element, target_element)
+    action.perform()
 
 
 drag_and_drop(SOURCE_LOCATOR, TARGET_LOCATOR)  # Использование функции
 
 # Отпускаем кнопку мыши
+
+# Локатор для элемента, который будем перетаскивать
+SOURCE_LOCATOR = ("xpath", "//div[@class='grid__item'][7]")
+# Локатор для элемента, куда будем перетаскивать
+TARGET_LOCATOR = ("xpath", "//div[@class='drop-area__item'][2]")
+# Открываем целевую страницу
 driver.get("https://tympanus.net/Development/DragDropInteractions/index.html")
-
-source = driver.find_element("xpath", "//div[@class='grid__item'][7]")  # Что перетаскиваем
-target = driver.find_element("xpath", "//div[@class='drop-area__item'][2]")  # Куда перетаскиваем
-
-actions.click_and_hold(source)
-actions.pause(2)
-actions.move_to_element(target)
-actions.release()
-actions.perform()
+# Находим элементы
+SOURCE = driver.find_element(*SOURCE_LOCATOR)
+TARGET = driver.find_element(*TARGET_LOCATOR)
+action.click_and_hold(SOURCE)  # Удерживаем кнопку мыши на source элементе
+action.pause(2)  # Делаем паузу в 2 секунды
+action.move_to_element(TARGET)  # Перемещаем курсор к target элементу
+action.release()  # Отпускаем кнопку мыши
+action.perform()  # Выполняем все действия
