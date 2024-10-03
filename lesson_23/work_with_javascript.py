f263720e2e6d9d07e26cb71658f13b42b6ac6517
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-10-03 15:57:44 +0300

    Work with Javascript and Scrolls

--- lesson_23/work_with_javascript.py
+++ lesson_23/work_with_javascript.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
 
