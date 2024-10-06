d0e08c4ad0a5718341b9ff33eeb71b6225294320
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-10-06 17:47:34 +0300

    Cookie handling, comments added

--- lesson_14/cookie_handling.py
+++ lesson_14/cookie_handling.py
@@ -6,54 +6,74 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 
 
+# Устанавливаем ChromeDriver через менеджер и создаем сервис
 service = Service(executable_path=ChromeDriverManager().install())
+# Инициализируем веб-драйвер для Chrome с заданными сервисом
 driver = webdriver.Chrome(service=service)
+# Устанавливаем ожидание до 15 секунд, проверяя наличие элемента каждую секунду
 wait = WebDriverWait(driver, 15, poll_frequency=1)
 
-# driver.get("https://www.freeconferencecall.com/login")
-# print(driver.get_cookie("country_code"))
-# print(driver.get_cookies())
-# driver.add_cookie({
-#     "name": "test_name",
-#     "value": "test_value"
-# })
-# print(driver.get_cookie("test_name"))
-
-# cookie_before = driver.get_cookie("split")
-# print(cookie_before)
-# driver.delete_cookie("split")
-# driver.add_cookie({
-#      "name": "split",
-#      "value": "test_split"
-# })
-# cookie_after = driver.get_cookie("split")
-# print(cookie_after)
-
-# cookies_before = driver.get_cookies()
-# print(cookies_before)
-# driver.delete_all_cookies()
-# driver.add_cookie({
-#      "name": "test_name",
-#      "value": "test_value"
-# })
-# cookies_after = driver.get_cookies()
-# print(cookies_after)
-
-# LOGIN_FIELD = ("xpath", "//input[@id='login_email']")
-# PASSWORD_FIELD = ("xpath", "//input[@id='password']")
-# SUBMIT_BUTTON = ("xpath", "//button[@id='loginformsubmit']")
-
-# driver.get("https://www.freeconferencecall.com/login")
-# driver.find_element(*LOGIN_FIELD).send_keys("login")
-# driver.find_element(*PASSWORD_FIELD).send_keys("password")
-# driver.find_element(*SUBMIT_BUTTON).click()
-# pickle.dump(driver.get_cookies(), open(os.getcwd() + "\\cookies\\cookies.pkl", "wb"))
-
+# Переходим на страницу входа
 driver.get("https://www.freeconferencecall.com/login")
-driver.delete_all_cookies()
+# Получаем и выводим cookie, чтобы проверить, установлены ли они
+print(driver.get_cookie("country_code"))  # Выводим cookie "country_code"
+print(driver.get_cookies())  # Выводим все cookies
+# Добавляем тестовую cookie
+driver.add_cookie({
+    "name": "test_name",
+    "value": "test_value"
+})
+print(driver.get_cookie("test_name"))  # Проверяем, была ли cookie добавлена
+
+# Переходим на страницу входа
+driver.get("https://www.freeconferencecall.com/login")
+# Работа с cookie "split"
+cookie_before = driver.get_cookie("split")  # Получаем cookie "split"
+print(cookie_before)  # Выводим значение cookie до удаления
+driver.delete_cookie("split")  # Удаляем cookie "split"
+# Добавляем новую cookie "split"
+driver.add_cookie({
+     "name": "split",
+     "value": "test_split"
+})
+cookie_after = driver.get_cookie("split")  # Получаем обновленную cookie
+print(cookie_after)  # Выводим значение cookie после добавления
+
+# Переходим на страницу входа
+driver.get("https://www.freeconferencecall.com/login")
+# Получаем все cookies перед удалением
+cookies_before = driver.get_cookies()
+print(cookies_before)  # Выводим cookies
+driver.delete_all_cookies()  # Удаляем все cookies
+# Снова добавляем тестовую cookie
+driver.add_cookie({
+     "name": "test_name",
+     "value": "test_value"
+})
+cookies_after = driver.get_cookies()  # Получаем cookies после добавления
+print(cookies_after)  # Выводим cookies
+
+# Локаторы для полей ввода и кнопки на странице
+LOGIN_FIELD = ("xpath", "//input[@id='login_email']")
+PASSWORD_FIELD = ("xpath", "//input[@id='password']")
+SUBMIT_BUTTON = ("xpath", "//button[@id='loginformsubmit']")
+# Переходим на страницу входа
+driver.get("https://www.freeconferencecall.com/login")
+# Вводим логин и пароль
+driver.find_element(*LOGIN_FIELD).send_keys("test_login")
+driver.find_element(*PASSWORD_FIELD).send_keys("test_password")
+driver.find_element(*SUBMIT_BUTTON).click()
+# Сохраняем cookies в файл для последующего использования
+pickle.dump(driver.get_cookies(), open(os.getcwd() + "\\cookies\\cookies.pkl", "wb"))
+
+# Переходим на страницу входа
+driver.get("https://www.freeconferencecall.com/login")
+driver.delete_all_cookies()  # Удаляем все cookies
+# Загружаем cookies из файла
 cookies = pickle.load(open(os.getcwd() + "\\cookies\\cookies.pkl", "rb"))
+# Добавляем cookies в браузер
 for cookie in cookies:
     driver.add_cookie(cookie)
 
-driver.refresh()
-# driver.get("https://www.freeconferencecall.com/profile")
+driver.refresh()  # Обновляем страницу, чтобы применить cookies
+driver.get("https://www.freeconferencecall.com/profile")  # Переходим на страницу профиля
