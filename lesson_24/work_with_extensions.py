0f435e1bcfe56904dbff6e917b981e33159f69ef
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-10-06 16:03:55 +0300

    Work with extensions

---
+++ lesson_24/work_with_extensions.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Создаем объект Options для настройки Chrome
+options = Options()
+options.add_argument("--window-size=1920,1080")  # Устанавливаем размер окна браузера
+options.add_extension('extensions\\adblock.crx')  # Устанавливаем расширение
+
+driver = webdriver.Chrome(options=options)
+
+# Открываем тестовую страницу
+driver.get("https://test.adminforge.de/adblock.html")
