4bc3559a1d99bc122ea847a085a08f0ae3a0eb3b
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-10-07 21:55:26 +0300

    Multi user implementation, comments added

--- lesson_19/multi_user_implementation.py
+++ lesson_19/multi_user_implementation.py
@@ -1,23 +1,20 @@
 import os
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 
-# Создаем объект Options для настройки Chrome
-options = Options()
+# Создаем объект ChromeOptions для настройки браузера
+options = webdriver.ChromeOptions()
 options.add_argument("--window-size=1920,1080")  # Устанавливаем размер окна браузера
-# Устанавливаем ChromeDriver через менеджер и создаем сервис
-service = Service(ChromeDriverManager().install())
-# Запускаем экземпляр Chrome для первого пользователя
-driver_user_1 = webdriver.Chrome(options=options, service=service)
+# Создаем сервис для Chrome
+service = webdriver.ChromeService()
+# Инициализируем веб-драйвер для Chrome с заданными сервисом и опциями
+driver_user_1 = webdriver.Chrome(service=service, options=options)
 
+# Открываем страницу логина
+driver_user_1.get("https://hyperskill.org/login")
 # Локаторы элементов
 LOGIN_FIELD = ("xpath", "//input[@type='email']")
 PASSWORD_FIELD = ("xpath", "//input[@type='password']")
 SUBMIT_BUTTON = ("xpath", "//button[@type='submit']")
-# Открываем страницу логина
-driver_user_1.get("https://hyperskill.org/login")
 # Вводим логин из переменной окружения
 driver_user_1.find_element(*LOGIN_FIELD).send_keys(os.environ["LOGIN_L19"])
 # Вводим пароль из переменной окружения
