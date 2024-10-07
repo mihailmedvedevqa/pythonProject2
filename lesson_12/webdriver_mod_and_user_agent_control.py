103abc3fdaee3ac57c5c6842e379b0395c5d0183
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-10-07 14:40:17 +0300

    Webdriver mod and user agent control, comments added

--- lesson_12/webdriver_mod_and_user_agent_control.py
+++ lesson_12/webdriver_mod_and_user_agent_control.py
@@ -1,23 +1,30 @@
-import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 
-
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-chrome_options.add_argument("--user-agent=Selenium")
-
-service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=chrome_options)
+# Создаем объект Options для настройки Chrome
+options = Options()
+options.add_argument("--window-size=1920,1080")  # Устанавливаем размер окна браузера
+# Отключаем песочницу, что может помочь при запуске в некоторых окружениях
+options.add_argument("--no-sandbox")
+# Отключаем использование общего пространства памяти, чтобы избежать ошибок в контейнерах
+options.add_argument("--disable-dev-shm-usage")
+# Скрываем информацию о том, что браузер управляется автоматизацией
+options.add_argument("--disable-blink-features=AutomationControlled")
+# Устанавливаем пользовательский агент для обхода блокировок
+options.add_argument("--user-agent=Selenium")
+# Устанавливаем ChromeDriver через менеджер и создаем сервис
+service = Service(ChromeDriverManager().install())
+# Инициализируем веб-драйвер для Chrome с заданными сервисом и опциями
+driver = webdriver.Chrome(service=service, options=options)
+# Устанавливаем ожидание до 15 секунд, проверяя наличие элемента каждую секунду
 wait = WebDriverWait(driver, 15, poll_frequency=1)
 
-# driver.get("https://www.google.com/")
-# driver.save_screenshot("screenshot.png")
-
+# Открываем тестовую страницу
 driver.get("https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html")
-time.sleep(3)
+# Сохраняем изображение экрана в файл
+driver.save_screenshot("screenshot.png")
+
+
