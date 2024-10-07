563023f4949fc3ac496bb0559b6f52dbf230b53b
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-10-07 22:12:09 +0300

    Javascript and scrolls, comments added

--- lesson_23/javascript.py
+++ lesson_23/javascript.py
@@ -1,27 +1,23 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver import ActionChains
-from selenium.webdriver.chrome.options import Options
 from scrolls import Scrolls
 
-# Создаем объект Options для настройки Chrome
-options = Options()
+# Создаем объект ChromeOptions для настройки браузера
+options = webdriver.ChromeOptions()
 options.add_argument("--window-size=1920,1080")  # Устанавливаем размер окна браузера
-
-# Устанавливаем ChromeDriver через менеджер и создаем сервис
-service = Service(ChromeDriverManager().install())
+# Создаем сервис для Chrome
+service = webdriver.ChromeService()
 # Инициализируем веб-драйвер для Chrome с заданными сервисом и опциями
 driver = webdriver.Chrome(service=service, options=options)
 # Создаем объект ActionChains для выполнения действий с элементами на странице
-action = ActionChains(driver)
+actions = ActionChains(driver)
 # Инициализируем класс Scrolls с драйвером и действиями
-scrolls = Scrolls(driver, action)
+scrolls = Scrolls(driver, actions)
 
-# Локатор элемента
-EXAMPLE2_LOCATOR = ("xpath", "//h3[text()='Example 2: ']")
 # Открываем целевую страницу
 driver.get("https://seiyria.com/bootstrap-slider/")
+# Локатор элемента
+EXAMPLE2_LOCATOR = ("xpath", "//h3[text()='Example 2: ']")
 # Находим элемент на странице с помощью заданного локатора
 EXAMPLE2 = driver.find_element(*EXAMPLE2_LOCATOR)
 # Прокручиваем страницу к элементу
--- lesson_23/scrolls.py
+++ lesson_23/scrolls.py
@@ -1,8 +1,8 @@
 class Scrolls:
 
-    def __init__(self, driver, action):
+    def __init__(self, driver, actions):
         self.driver = driver
-        self.action = action
+        self.actions = actions
 
     def scroll_by(self, x, y):  # Scroll по x и y
         self.driver.execute_script(f"window.scrollTo({x}, {y})")
@@ -14,7 +14,7 @@ class Scrolls:
         self.driver.execute_script("window.scrollTo(0, 0)")
 
     def scroll_to_element(self, element):  # Scroll к элементу с раскрытием контента под ним
-        self.action.scroll_to_element(element).perform()
+        self.actions.scroll_to_element(element).perform()
         self.driver.execute_script("""
         window.scrollTo({
             top: window.scrollY + 700,
