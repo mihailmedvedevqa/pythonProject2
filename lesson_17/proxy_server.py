cb9db995f2c347074f58854547f49fdea61b3132
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-10-07 21:49:00 +0300

    Proxy server, comments added

--- lesson_17/proxy_server.py
+++ lesson_17/proxy_server.py
@@ -1,22 +1,18 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 
 # Указываем адрес прокси-сервера
 PROXY = "37.19.220.129:8443"
 # Указываем адрес прокси-сервера с авторизацией
 AUTH_PROXY = "username:password@37.19.220.129:8443"
-# Создаем объект Options для настройки Chrome
-options = Options()
+# Создаем объект ChromeOptions для настройки браузера
+options = webdriver.ChromeOptions()
 options.add_argument("--window-size=1920,1080")  # Устанавливаем размер окна браузера
 # Добавляем прокси через опции
 options.add_argument(f"--proxy-server={PROXY}")
-# Устанавливаем ChromeDriver через менеджер и создаем сервис
-service = Service(ChromeDriverManager().install())
+# Создаем сервис для Chrome
+service = webdriver.ChromeService()
 # Инициализируем веб-драйвер для Chrome с заданными сервисом и опциями
 driver = webdriver.Chrome(service=service, options=options)
-
 
 # Проверяем IP-адрес
 driver.get("https://whatismyipaddress.com/")
