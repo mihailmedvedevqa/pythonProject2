df39dc020d9c3e76cd2ff669aeb59c24a944e7bd
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-10-07 21:16:23 +0300

    Webdriver mod and user agent control, comments added

--- lesson_12/webdriver_mod_and_user_agent_control.py
+++ lesson_12/webdriver_mod_and_user_agent_control.py
@@ -1,11 +1,8 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 
-# Создаем объект Options для настройки Chrome
-options = Options()
+# Создаем объект ChromeOptions для настройки браузера
+options = webdriver.ChromeOptions()
 options.add_argument("--window-size=1920,1080")  # Устанавливаем размер окна браузера
 # Отключаем песочницу, что может помочь при запуске в некоторых окружениях
 options.add_argument("--no-sandbox")
@@ -15,8 +12,8 @@ options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-blink-features=AutomationControlled")
 # Устанавливаем пользовательский агент для обхода блокировок
 options.add_argument("--user-agent=Selenium")
-# Устанавливаем ChromeDriver через менеджер и создаем сервис
-service = Service(ChromeDriverManager().install())
+# Создаем сервис для Chrome
+service = webdriver.ChromeService()
 # Инициализируем веб-драйвер для Chrome с заданными сервисом и опциями
 driver = webdriver.Chrome(service=service, options=options)
 # Устанавливаем ожидание до 15 секунд, проверяя наличие элемента каждую секунду
