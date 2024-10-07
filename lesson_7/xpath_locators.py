08941591835911366a6bbd985c7476cbd2af0797
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-10-07 20:18:17 +0300

    Xpath locators, comments added

--- lesson_7/xpath_locators.py
+++ lesson_7/xpath_locators.py
@@ -1,11 +1,21 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-service = Service(executable_path=ChromeDriverManager().install())
+# Создаем сервис для Chrome
+service = webdriver.ChromeService()
+# Инициализируем веб-драйвер для Chrome с заданными сервисом и опциями
 driver = webdriver.Chrome(service=service)
 
+# Переходим на страницу
 driver.get("https://hyperskill.org/courses")
-
-SIGN_IN_BUTTON = driver.find_elements("xpath", "//button[contains(@class, 'btn-outline-light'")
-START_FOR_FREE_BUTTON = driver.find_elements("xpath", "//button[contains(@class, 'btn-primary'")
+# Ожидаем, пока кнопка "Войти" станет кликабельной
+SIGN_IN_BUTTON = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable(("xpath", "//button[contains(@class, 'btn-outline-light')]"))
+)
+SIGN_IN_BUTTON.click()  # Кликаем на кнопку
+# Ожидаем, пока кнопка "Начать бесплатно" станет кликабельной
+START_FOR_FREE_BUTTON = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable(("xpath", "//button[contains(@class, 'btn-primary')]"))
+)
+START_FOR_FREE_BUTTON.click()  # Кликаем на кнопку
