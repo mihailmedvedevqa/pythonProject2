10c688974b830070711d7358aabfdf7fc8f36e3a
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-10-07 21:28:05 +0300

    Checkboxes and radiobuttons, comments added

--- lesson_15/check_boxes_and_radio_buttons.py
+++ lesson_15/check_boxes_and_radio_buttons.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 
-# Создаем объект Options для настройки Chrome
-options = Options()
+# Создаем объект ChromeOptions для настройки браузера
+options = webdriver.ChromeOptions()
 options.add_argument("--window-size=1920,1080")  # Устанавливаем размер окна браузера
-# Устанавливаем ChromeDriver через менеджер и создаем сервис
-service = Service(ChromeDriverManager().install())
+# Создаем сервис для Chrome
+service = webdriver.ChromeService()
 # Инициализируем веб-драйвер для Chrome с заданными сервисом и опциями
 driver = webdriver.Chrome(service=service, options=options)
 
