15acfae6504e01aa3e2f73ebd6accd827392f133
Mihail Medvedev <mihail.medvedev.qa@gmail.com>
2024-10-07 20:39:03 +0300

    Data retrieval and validation, comments added

--- lesson_4/data_retrieval_and_validation.py
+++ lesson_4/data_retrieval_and_validation.py
@@ -1,23 +1,23 @@
-import time
-
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 
-service = Service(executable_path=ChromeDriverManager().install())
+# Создаем сервис для Chrome
+service = webdriver.ChromeService()
+# Инициализируем веб-драйвер для Chrome с заданными сервисом и опциями
 driver = webdriver.Chrome(service=service)
 
+# Переходим на главную страницу Википедии
 driver.get("https://www.wikipedia.org/")
-
+# Получаем текущий URL страницы
 PAGE_URL = driver.current_url
 print(f"Page URL: {PAGE_URL}")
-assert PAGE_URL == "https://www.wikipedia.org/", "URL error, wrong page opened"
-
+# Проверяем, что URL соответствует ожидаемому
+assert PAGE_URL == "https://www.wikipedia.org/", "Ошибка URL, открыта не та страница"
+# Получаем заголовок страницы
 PAGE_TITLE = driver.title
 print(f"Page Title: {PAGE_TITLE}")
-assert PAGE_TITLE == "Wikipedia", "Incorrect page title"
-
+# Проверяем, что заголовок соответствует ожидаемому
+assert PAGE_TITLE == "Wikipedia", "Неправильный заголовок страницы"
+# Получаем исходный код страницы
 PAGE_SOURCE = driver.page_source
 print(PAGE_SOURCE)
 
-time.sleep(3)
